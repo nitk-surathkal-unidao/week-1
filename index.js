0d0c945f066d7f05d782dfc424d056a90e297fee
bhaveshbillionaier19 <bhaveshkumarsharmaengineer@gmail.com>
2024-09-10 00:14:02 +0530

    Implement findColor function

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
 // given a hash, return the color that created the hash
 function findColor(hash) {
     // code here
+    for (let i of COLORS) {
+        
+        const iB = utf8ToBytes(i);
+        const iH = sha256(iB);
+        const ihex = toHex(iH);
+        if (ihex === toHex(hash)) {
+            return i; 
+        }
+    }
+    
+    return null;
 }
 
 module.exports = findColor;
