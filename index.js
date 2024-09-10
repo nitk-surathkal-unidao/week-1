1cad7cd5fae52f244d514a51dc3d8a76c3cf55ba
Shivam <sk8625228@gmail.com>
2024-09-11 02:44:22 +0530

    Implement findColor function

--- index.js
+++ index.js
@@ -5,20 +5,43 @@ const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
 // the possible colors that the hash could represent
 var COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
 
-// given a hash, return the color that created the hash
+
 const colorHashMap = new Map();
     COLORS.forEach(colr => { const hash = toHex(sha256(utf8ToBytes(colr)));
         colorHashMap.set(hash, colr);
     });
     
-    
+
     
     function findColor(hash) {
         //code here
-        //coding the above map in this section will unnecessarily redeclare the map
-        
         return colorHashMap.get(toHex(hash)) || null;
     }
 
+        // function to generate a random salt of a given length
+    function generateSalt() {
+        const saltLength = 16;
+        const array = new Uint8Array(saltLength);
+        crypto.getRandomValues(array)
+        return toHex(array);
+    }
 
-module.exports = findColor;
+    /** Salting the array element makes it very difficult for unauthorised person to guess/find the input */
+
+    const colorWithSalt = new Map();
+    const colors=COLORS.copyWithin()
+    colors.forEach(COLOR => {
+    const salt = generateSalt();                                // generate a random salt
+    const hash = toHex(sha256(utf8ToBytes(COLOR+salt)));
+        colorHashMap.set(hash, COLOR+salt);
+    });
+
+    function findColor_in_colorWithSalt(hash) {
+        return colorWithSalt.get(toHex(hash)) || null;
+    }
+
+
+
+module.exports = {findColor, 
+    findColor_in_colorWithSalt
+}
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const {assert} = require('chai');
-const findColor = require('./index');
+const { findColor, findColor_in_colorWithSalt } = require('./index');
 const { sha256 } = require("ethereum-cryptography/sha256");
 const { utf8ToBytes } = require("ethereum-cryptography/utils");
 
@@ -7,13 +7,14 @@ const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
 const COLORS_salt=['red']
 describe('findColor', () => {
 
-
+//_____testing for the same COLORS array___________________________________________
     COLORS.forEach((color) => {
         it(`should work for ${color}`, () => {
-            
             assert.equal(findColor(sha256(utf8ToBytes(color))), color);
         });
     });
+
+//_____testing with a unknown random input__________________________________________
 
     // if the given input which isn't in the defined COlors array , it should return null
     it('should return null for a hash that does not correspond to any color', () => {
@@ -26,16 +27,30 @@ describe('findColor', () => {
     * meaning that the binary representation of each character is different depending on its case
     * */
     
+//____testing case-sensitivity______________________________________________________
+
     const hash = sha256(utf8ToBytes('Green'));                     //Shouldn't work for Capital 'G' 
-    it(`should also work for Green`, () => {
+    it(`should also work for Green (checking case-sensitivity)`, () => {
         try{
-        assert.equal(findColor(hash) || 'Green', 'green');         //catch an AssertionError 
+        assert.equal(findColor(hash) || 'Green', 'green');        
         throw new Error('invalid color')
     }catch (error) {
         console.error("Error finding color:", error.message);  
     }
 });
 
-
-  
+//_____testing with salted array______________________________________________________
+COLORS.forEach((color) => {
+    it(`Does this work for ${color} ?`, () => {
+        try{
+            assert.equal(findColor_in_colorWithSalt(hash) || color+'+ kuch randomsalt' , color);        
+            throw new Error('invalid color')
+        }catch (error) {
+            console.error("Error finding color:", error.message);  
+        }
+    });
 });
+
+
+});
+  
