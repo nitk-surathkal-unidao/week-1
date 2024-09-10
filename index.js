e28f4d6fac6d903b7e7a7932617cec17eec878e7
Prakhar Rakhya <165322550+prakharrakhya@users.noreply.github.com>
2024-09-10 21:35:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
 
 // given a hash, return the color that created the hash
 function findColor(hash) {
-    // code here
+    for(let i =0 ; i<COLORS.length ; i++){
+        temp = utf8ToBytes(COLORS[i])
+        temp2  = sha256(temp);
+        cmp1 = toHex(hash);
+        cmp2 = toHex(temp2);
+        if (cmp2 == cmp1){
+            return COLORS[i];
+        }
+    }
 }
 
 module.exports = findColor;
