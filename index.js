8917b89f6522ff197ff6ff3e8eda1d983e375cd1
Shivam <sk8625228@gmail.com>
2024-09-10 09:18:21 +0530

    Implement findColor function

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const { sha256 } = require("ethereum-cryptography/sha256");
 const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
 
+// // the possible colors that the hash could represent
+// var COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
+
+// // given a hash, return the color that created the hash
+
+// function findColor() {
+//     COLORS = COLORS.map(colr=>(sha256(utf8ToBytes(colr)) ) )
+//     console.log(COLORS)
+// }
+// findColor();
+
+
+
 // the possible colors that the hash could represent
-const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
+var COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
 
 // given a hash, return the color that created the hash
-function findColor(hash) {
-    // code here
-}
+const colorHashMap = new Map();
+    COLORS.forEach(colr => { const hash = toHex(sha256(utf8ToBytes(colr)));
+        colorHashMap.set(hash, colr);
+    });
+    
+    
+    function findColor(hash) {
+//code here
+        //coding the above map in this section will unnecessarily redeclare the map
+        return colorHashMap.get(toHex(hash)) || null;
+    }
+
 
 module.exports = findColor;
