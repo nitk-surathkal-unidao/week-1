6492a31b77953fc2111534e6b8ac959950c72952
Varshini <varshini.231cs204@nitk.edu.in>
2024-09-10 20:21:36 +0530

    Implement findColor function

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
 
 // given a hash, return the color that created the hash
 function findColor(hash) {
-    // code here
+    for(let color of COLORS){
+        let keyByte = utf8ToBytes(color);
+        let key = sha256(keyByte);
+        if(toHex(key)===toHex(hash)){
+            return color;
+        }
+    }
 }
 
 module.exports = findColor;
