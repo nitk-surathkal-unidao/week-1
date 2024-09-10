8ae44a671b7563ca6144ca244fd31a48641d3e75
Shivam <sk8625228@gmail.com>
2024-09-11 02:18:04 +0530

    Implement findColor function

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const { sha256 } = require("ethereum-cryptography/sha256");
 const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
 
-// // the possible colors that the hash could represent
-// var COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
-
-// // given a hash, return the color that created the hash
-
-// function findColor() {
-//     COLORS = COLORS.map(colr=>(sha256(utf8ToBytes(colr)) ) )
-//     console.log(COLORS)
-// }
-// findColor();
-
-
 
 // the possible colors that the hash could represent
 var COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
@@ -24,9 +12,11 @@ const colorHashMap = new Map();
     });
     
     
+    
     function findColor(hash) {
-//code here
+        //code here
         //coding the above map in this section will unnecessarily redeclare the map
+        
         return colorHashMap.get(toHex(hash)) || null;
     }
 
--- test.js
+++ test.js
@@ -4,11 +4,38 @@ const { sha256 } = require("ethereum-cryptography/sha256");
 const { utf8ToBytes } = require("ethereum-cryptography/utils");
 
 const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
-
+const COLORS_salt=['red']
 describe('findColor', () => {
+
+
     COLORS.forEach((color) => {
         it(`should work for ${color}`, () => {
+            
             assert.equal(findColor(sha256(utf8ToBytes(color))), color);
         });
     });
+
+    // if the given input which isn't in the defined COlors array , it should return null
+    it('should return null for a hash that does not correspond to any color', () => {
+        const randomHash = sha256(utf8ToBytes('koi_bhi_random_input'));
+        assert.equal(findColor(randomHash), null);
+    });
+
+
+    /** UTF-8 encoding (which utf8ToBytes uses) works at the byte level,
+    * meaning that the binary representation of each character is different depending on its case
+    * */
+    
+    const hash = sha256(utf8ToBytes('Green'));                     //Shouldn't work for Capital 'G' 
+    it(`should also work for Green`, () => {
+        try{
+        assert.equal(findColor(hash) || 'Green', 'green');         //catch an AssertionError 
+        throw new Error('invalid color')
+    }catch (error) {
+        console.error("Error finding color:", error.message);  
+    }
+});
+
+
+  
 });
