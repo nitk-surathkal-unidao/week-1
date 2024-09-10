c0b2e57e4855b1b7759b0d15f518015f73d261e8
keyabist <keyabist.231ee226@nitk.edu.in>
2024-09-10 07:26:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
 // the possible colors that the hash could represent
 const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
 
+
 // given a hash, return the color that created the hash
 function findColor(hash) {
     // code here
+    for (i in COLORS) {
+        if (toHex(sha256(utf8ToBytes(COLORS[i]))) == toHex(hash)) {
+            return COLORS[i]
+        }
+    }
+
 }
 
 module.exports = findColor;
