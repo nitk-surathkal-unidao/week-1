f4cf2c06ac289c25ca286253fe8cf34535e837f2
Nanda Kishor Vinod <nandakishorvinod2020@gmail.com>
2024-09-11 22:46:39 +0530

    Implement find color function for week-1 unidao

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
 // the possible colors that the hash could represent
 const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
 
+//declaring a map to create the rainbow table
+const rainbowTable=new Map();
 // given a hash, return the color that created the hash
+//filling the  Rainbow Table
+for(let i=0;i<COLORS.length;i++){
+    let toByte=utf8ToBytes(COLORS[i]);
+    let SHA=sha256(toByte);
+    rainbowTable.set(toHex(SHA),COLORS[i]);
+}
+
 function findColor(hash) {
-    // code here
+    //checking if the corresponding entry exists
+    if(rainbowTable.get(toHex(hash))){
+        return rainbowTable.get(toHex(hash));
+    }
 }
 
 module.exports = findColor;
