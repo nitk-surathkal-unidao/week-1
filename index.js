aab36cf2fce498d44a2b8b1a799108aa0289d5c6
GhostBYte <123203175+sujalnitk@users.noreply.github.com>
2024-09-11 12:33:24 +0000

    Implement findColor function

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
 
 // given a hash, return the color that created the hash
 function findColor(hash) {
-    // code here
+    for (let color of COLORS) {
+        const colorBytes = utf8ToBytes(color); // bytes conversion
+        const colorHash = sha256(colorBytes);
+        const colorHex = toHex(colorHash);
+
+        if (colorHex == toHex(hash)){
+            return color;
+        }
+
+    }
+    return null;
 }
 
 module.exports = findColor;
